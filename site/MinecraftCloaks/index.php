0aac3b8825d748765d8bfc3f688957649c703c64
zenit <zenit@m.ru>
2015-04-14 19:51:06 +0300

    FixInvalidCloacks final

--- site/MinecraftCloaks/index.php
+++ site/MinecraftCloaks/index.php
@@ -1,15 +1,15 @@
 <?php
 foreach($_REQUEST as $key => $val)
-$path = dirname(__FILE__);
 $imgBig = '1.png';
-$imgSmall = str_replace("$", "", $key.'.png');
-@$img1 = imagecreatefrompng($path . DIRECTORY_SEPARATOR . $imgBig);
-@$img2 = imagecreatefrompng($path . DIRECTORY_SEPARATOR . $imgSmall);
+$imgSmall = str_replace(array('$', '/'), array('', ''), $key.'.png');
+@$img1 = imagecreatefrompng($imgBig);
+@$img2 = imagecreatefrompng($imgSmall);
 if($img1 and $img2) {
 	imageSaveAlpha($img1, true);
 	imageSaveAlpha($img2, true);
 	header('Content-Type: image/png');
-	if(getimagesize($imgSmall)[0] == 22) {
+	$size = getimagesize($imgSmall);
+	if($size[0] == 22) {
 		$x2 = imagesx($img2);
 		$y2 = imagesy($img2);
 		imagecopyresampled(
